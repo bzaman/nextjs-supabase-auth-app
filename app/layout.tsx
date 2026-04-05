cfa352b22cc0243c5e88ce3eef761639b64507ab
Badiuzzaman <folkgraphein@gmail.com>
2026-04-05 15:38:36 +0600

    RootLayout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,31 @@
 import "./globals.css";
 
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import { Roboto_Condensed, Playfair_Display } from "next/font/google";
+
+import { APP_NAME, APP_DESC, SERVER_URL } from "@/lib/constants";
+import { cn } from "@/lib/utils";
+
+const playfair_display = Playfair_Display({
+  variable: "--font-playfair-display",
+  subsets: ["latin"],
+});
+
+const roboto_condensed = Roboto_Condensed({
+  variable: "--font-roboto-condensed",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | Auth App`,
+    default: APP_NAME,
+  },
+  description: APP_DESC,
+  metadataBase: new URL(SERVER_URL),
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,8 +34,25 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      data-scroll-behavior="smooth"
+      className={cn([
+        playfair_display.variable,
+        roboto_condensed.variable,
+        "antialiased",
+        "scroll-smooth",
+      ])}
+      suppressHydrationWarning
     >
-      <body>{children}</body>
+      <body className="min-h-svh">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
