68828d479de88807022f89d03e813572f55791a0
Badiuzzaman <folkgraphein@gmail.com>
2026-04-05 17:14:44 +0600

    hope update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full max-w-3xl mx-auto">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+       <p className="text-lg tracking-wide"><Link href="/email-password">email-password</Link></p>
+       <p className="text-lg tracking-wide"><Link href="/google-login">Register</Link></p>
+      </main>
     </div>
   );
 }
