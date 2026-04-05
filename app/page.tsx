44d0f9f65e88e2dc7c30e545e7c765eb617cc017
Badiuzzaman <folkgraphein@gmail.com>
2026-04-05 17:50:42 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="w-full max-w-3xl mx-auto">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start">
        <p className="text-lg tracking-wide"><Link href="/email-password">email-password</Link></p>
        <p className="text-lg tracking-wide"><Link href="/google-login">Register</Link></p>
+       <h1 className="text-5xl font-bold">Test</h1>
+       <h2 className="text-3xl font-sans-serif">another test</h2>
       </main>
     </div>
   );
